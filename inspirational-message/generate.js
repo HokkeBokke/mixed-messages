584a025b147f05d1975d5ff57677f7351dbedd0d
Håkon <haakon@oisund.no>
2023-10-11 17:52:54 +0200

    New dataset and script getting it + changes on how quote is displayed

--- inspirational-message/generate.js
+++ inspirational-message/generate.js
@@ -1,15 +1,19 @@
-const MESSAGE_FILE = 'messages.txt';
+const MESSAGE_FILE = './quotes.csv';
 const quoteElem = document.getElementById('quote');
 const BUTTON = document.querySelector('button');
+const AUTHOR = 0;
+const QUOTE = 1;
 
-async function generateQuote() {
+async function getQuote() {
   let res = await fetch(MESSAGE_FILE);
   let content = await res.text();
-  let quotes = content.split("----");
-  
-  let randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+  let lines = content.split('\n');
+  lines.splice(0,1);
 
-  quoteElem.textContent = randomQuote;
+  let randomQuote = lines[Math.floor(Math.random()*lines.length)].split(',');
+  let finalQuote = `"${randomQuote[QUOTE]}"\n- ${randomQuote[AUTHOR]}`;
+  
+  quoteElem.textContent = finalQuote;
 }
 
-BUTTON.addEventListener('click', generateQuote);
+BUTTON.addEventListener('click', getQuote);
